031fb88b102460ccdaf27f8a43d5ab25cdbf172c
Anuj-kumarr <98229406+Anuj-kumarr@users.noreply.github.com>
2024-12-28 07:12:33 +0000

    datatypes-sumaary and comparison

---
+++ basics/comparison.js
@@ -0,0 +1,6 @@
+//comparison operator like >=,>,<,<= aur ===,== diffeenet tareeke e kaam karte hai jab null ya undefined aata hai  to
+console.log(null>0)
+console.log(null<0)
+console.log(null==0)
+console.log(null<=0)//null ko 0 me convert kar dete hai comparison lekin == nhi kar pata 
+//upar waale compaison ko hum avoid hi karte hai humesha
---
+++ datatypes-summary.js
@@ -0,0 +1,24 @@
+//data kese memory me store hota hai ar kese access kiajjata  hai uske basis par do  types ke datatype hote hai..primitive and nonprimitive
+//jaise c++ me call by value aur caal by refeence hote hai wse hi
+
+
+// #primitive datatypes
+//1.call by value hota hai ye
+//2.7 types ka hota hai jaise ki .....number,string,null,undefined,boolean,symbol(kisi bhi value ko unique banane ke lie use hota hai),bigint
+
+
+
+//# Nonprimitive data types(Reference type)
+// arrays,objects,functions
+
+
+const id=Symbol('123')
+const dosriid=Symbol('123')
+console.log(id)
+console.log(dosriid)
+console.log(id===dosriid)
+
+
+
+
+
