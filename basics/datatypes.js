295a9cdd3cc7530b7c218c4edc1117a5dbe2248c
Anuj-kumarr <98229406+Anuj-kumarr@users.noreply.github.com>
2024-12-28 05:34:34 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,22 @@
+// alert("aloo")
+//var ko nhi use karenge kyuki scope ki dikkat hoti hai
+// let anuj=16;
+// if(true)
+// {
+//      let anuj=14
+//     console.log(anuj)
+// }
+// console.log(anuj)
+var anuj=15
+console.log(anuj)
+  anuj=11
+console.log(anuj)
+//undefined and null ke beech mein difference bataya hai
+let val;
+let value=null
+console.table([val,value])
+//types of datatype in js
+//number,null.undefined,string,boolean,
+console.log(typeof null)
+console.log(typeof undefined)
+//null ka type object hota hai 
---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const id=122
+let email="anuj@gmail.com"
+console.log(typeof(id))
+console.log(typeof(email))
+city="jai"
+// console.log(typeof(city))
+console.table([id,email,city])
+console.table([id,email,city])
