7d4b634fbcf8151e376d2a748666bd795f3b0219
Anuj-kumarr <98229406+Anuj-kumarr@users.noreply.github.com>
2024-12-28 05:54:35 +0000

    conversion of datatypes

---
+++ basics/conzversionoperation.js
@@ -0,0 +1,12 @@
+// let score=99
+// let val=String(score)
+// console.log(typeof val)
+// let no="33abc"
+// let anu=Number(no)
+// console.log(typeof anu)
+// console.log(anu)
+//boolean conversion
+let val=1;
+let ans=Boolean(val)
+console.log(typeof(ans))
+console.log(ans)
