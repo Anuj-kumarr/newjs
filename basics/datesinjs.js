b4d4d90759217a5fd7472f1553202d5df75a6ff6
Anuj-kumarr <98229406+Anuj-kumarr@users.noreply.github.com>
2024-12-28 17:10:37 +0000

    datesinjs

---
+++ basics/datesinjs.js
@@ -0,0 +1,22 @@
+//Dates
+
+//dates object hoti hai-------------------
+
+// const date=new Date()
+// console.log(date.toLocaleString())
+// console.log(date.toString())
+// console.log(date.toDateString())
+
+// let datenew=new Date("2024-12-28")
+// console.log(datenew.toLocaleString())
+
+// let stamp=Date.now();
+// console.log(stamp)
+// console.log(datenew.getTime())
+
+let newDate=new Date();
+console.log(newDate)
+console.log(newDate.getDay())
+console.log(newDate.getMonth()+1)
+
+
